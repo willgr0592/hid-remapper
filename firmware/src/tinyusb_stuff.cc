8213e9f1a1b11fc42d6aed8314e3a7d53ec5b38e
willgr0592 <williamguidolin@gmail.com>
2026-03-06 19:49:28 -0300

    Change USB VID and PID to Logitech values
    Updated USB vendor and product IDs for Logitech.

--- firmware/src/tinyusb_stuff.cc
+++ firmware/src/tinyusb_stuff.cc
@@ -34,8 +34,10 @@
 
 // These IDs are bogus. If you want to distribute any hardware using this,
 // you will have to get real ones.
-#define USB_VID 0xCAFE
-#define USB_PID 0xBAF2
+//#define USB_VID 0xCAFE
+//#define USB_PID 0xBAF2
+#define USB_VID 0x046D  // Logitech
+#define USB_PID 0xC24E  // G600
 
 tusb_desc_device_t desc_device = {
     .bLength = sizeof(tusb_desc_device_t),
