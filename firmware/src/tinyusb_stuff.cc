83a9e9ee6ecfc1e547ccb129ab80eed8a14f5498
willgr0592 <williamguidolin@gmail.com>
2026-03-06 23:21:18 -0300

    Update tinyusb_stuff.cc

--- firmware/src/tinyusb_stuff.cc
+++ firmware/src/tinyusb_stuff.cc
@@ -22,9 +22,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- *//
+ */
 
-#include <tusb.h>
+#include <tusb.h> //
 
 #include "config.h"
 #include "globals.h"
